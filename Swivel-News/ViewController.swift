804768f69cb7e7299382cf7250ade757448d227e
Harsha Harischandra <harsha@virtualcapital.co.nz>
2019-12-10 10:16:00 +0530

    Initial Commit

---
+++ Swivel-News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swivel-News
+//
+//  Created by Harsha Kuruwita on 12/10/19.
+//  Copyright © 2019 Harsha Kuruwita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swivel-NewsTests/Swivel_NewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swivel_NewsTests.swift
+//  Swivel-NewsTests
+//
+//  Created by Harsha Kuruwita on 12/10/19.
+//  Copyright © 2019 Harsha Kuruwita. All rights reserved.
+//
+
+import XCTest
+@testable import Swivel_News
+
+class Swivel_NewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
