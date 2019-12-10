0770d12b5d0c42fec5a5df3d9c03aaa18a527c2d
Harsha Harischandra <harsha@virtualcapital.co.nz>
2019-12-10 10:22:48 +0530

    Init

---
+++ Pods/Target Support Files/Pods-Swivel-News-Swivel-NewsUITests/Pods-Swivel-News-Swivel-NewsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swivel_News_Swivel_NewsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Swivel_News_Swivel_NewsUITests
+@end
---
+++ Pods/Target Support Files/Pods-Swivel-NewsTests/Pods-Swivel-NewsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Swivel_NewsTests : NSObject
+@end
+@implementation PodsDummy_Pods_Swivel_NewsTests
+@end
